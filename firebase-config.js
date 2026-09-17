bab89c9f0a6d27edfca0463d763b159a946a6b6c
PRASA8591 <punsaraprasanna@gmail.com>
2026-09-17 14:59:20 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyA3fhxeSNb8MBqkmSsA55XCZaziRQm_FPk",
   authDomain: "inventory-new-379dc.firebaseapp.com",
@@ -13,10 +8,6 @@ const firebaseConfig = {
   measurementId: "G-728ZE17NHY"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Initialize Services & Export
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
